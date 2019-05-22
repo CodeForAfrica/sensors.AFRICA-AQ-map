a807320f41559eba033a691a18ee9799eee802f9
nerdlet <brndnyokabi@gmail.com>
2019-05-22 15:35:13 +0300

    Remove location

--- src/cell-info.vue
+++ src/cell-info.vue
@@ -17,7 +17,6 @@
 			th PM10 µg/m³
 			th PM2.5 µg/m³
 			th Last actisve date
-			th Location
 		tr.mean
 			td mean
 			td {{mean.P1.toFixed(0)}}
@@ -32,7 +31,7 @@
 				td {{sensor.o.data.P1.toFixed(0)}}
 				td {{sensor.o.data.P2.toFixed(0)}}
 				td {{sensor.o.date}}
-				td {{sensor.o.location}}
+				
 			tr(:id = "'graph_'+sensor.o.id" style="display:none" class="cell_info_images")
 				td(:id = "'images_'+sensor.o.id" colspan='6')
 					br
@@ -43,7 +42,6 @@ import _ from "lodash";
 export default {
   data() {
     return {
-      location: (this.cell, o => o.o.location),
       date: (this.cell, o => o.o.date)
     };
   },
